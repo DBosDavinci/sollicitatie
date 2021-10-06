5f250cefb9db900628e24042c3eac61808fbd3a0
DBosDavinci <99069008@mydavinci.nl>
2021-10-06 09:45:58 +0200

    sollicitatie-redo

--- sollicitatie.py
+++ sollicitatie.py
@@ -1,68 +1,23 @@
 dierendressuur = int(input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?"))
 jongleren = int(input("Hoeveel jaar ervaring heeft u met jongleren?"))
 acrobatiek = int(input("Hoeveel jaar praktijkervaring heeft u met acrobatiek?"))
-
-if dierendressuur >= 4 or jongleren >= 5 or acrobatiek >= 3:
-    school = input("Bent u ooit naar school geweest?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if school == "ja":
-    diploma = input("Heeft u een diploma MBO-4 ondernemen?")
-else:
-    diploma = input("Heeft u een diploma MBO-4 ondernemen?")
-    
-if diploma == "ja":
-    posters = input("Heeft u thuis posters aan de muur hangen?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if posters == "ja":
-    rijbewijs = input("Heeft u een geldig vrachtwagen rijbewijs?")
-else:
-    rijbewijs = input("Heeft u een geldig vrachtwagen rijbewijs?")
-
-if rijbewijs == "ja":
-    kleur = input("Wat is uw lievelingkleur?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if kleur == "rood":
-    hoed = input("Bent u in bezit van een hoge hoed?")
-else:
-    hoed = input("Bent u in bezit van een hoge hoed?")
-
-if hoed == "ja":
-    gender = input("Bent u man of vrouw?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
+school = input("Bent u ooit naar school geweest?")
+diploma = input("Heeft u een diploma MBO-4 ondernemen?")
+posters = input("Heeft u thuis posters aan de muur hangen?")
+rijbewijs = input("Heeft u een geldig vrachtwagen rijbewijs?")
+kleur = input("Wat is uw lievelingkleur?")
+hoed = input("Bent u in bezit van een hoge hoed?")
+gender = input("Bent u man of vrouw?")
 if gender == "man":
     snor = int(input("Heeft u een snor en zo ja, hoe groot is deze in cm?"))
 elif gender == "vrouw":
     haar = int(input("Heeft u rood krulhaar en zo ja, hoe lang is uw haar in cm?"))
+lengte = int(input("Wat is uw lengte in cm?"))
+gewicht = input("Wat is uw lichaamsgewicht in kg?")
+zakjes = input("Heeft u thuis 15 boterhamzakjes?")
+certificaat = input("Heeft u het certificaat Overleven met gevaarlijk personeel?")
 
-if snor >= 10 or haar >= 20:
-    lengte = int(input("Wat is uw lengte in cm?"))
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if lengte >= 150:
-    gewicht = input("Wat is uw lichaamsgewicht in kg?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if gewicht >= 90:
-    zakjes = input("Heeft u thuis 15 boterhamzakjes?")
-else:
-    print("Sorry, u voldoet niet aan de criteria van deze baan")
-
-if zakjes == 0:
-    certificaat = input("Heeft u het certificaat Overleven met gevaarlijk personeel?")
-else:
-    certificaat = input("Heeft u het certificaat Overleven met gevaarlijk personeel?")
-
-if certificaat == "ja":
+if dierendressuur >= 4 or jongleren >= 5 or acrobatiek >= 3 and diploma == "ja" and rijbewijs == "ja" and hoed == "ja" and snor >= 10 or haar >= 20 and lengte >= 150 and gewicht >= 90 and certificaat == "ja":
     print("Gefeliciteerd, u bent geschikt voor deze baan")
 else:
     print("Sorry, u voldoet niet aan de criteria van deze baan")
